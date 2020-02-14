220f4e212c5283b1f9f6f7b04ed9127c1342d1ae
Raheel Uppal <ruppal@zipcodes-MacBook-Pro.local>
2020-02-13 19:55:10 -0500

    final

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -16,18 +16,16 @@ public class Board {
     String dig1;
     String dig2;
 
-    ArrayList<String> lines;
+    private ArrayList<String> lines;
+
+
 
 
     public Board(Character[][] matrix) {
         this.board = matrix;
         row1 = board[0][0].toString() + board[0][1].toString() + board[0][2].toString();
-        row2 = board[1][0].toString();
-        board[1][1].toString();
-        board[1][2].toString();
-        row3 = board[2][0].toString();
-        board[2][1].toString();
-        board[2][2].toString();
+        row2 = board[1][0].toString() + board[1][1].toString() + board[1][2].toString();
+        row3 = board[2][0].toString() + board[2][1].toString() + board[2][2].toString();
 
         col1 = board[0][0].toString() + board[1][0].toString() + board[2][0].toString();
         col2 = board[0][1].toString() + board[1][1].toString() + board[2][1].toString();
@@ -36,7 +34,7 @@ public class Board {
         dig1 = board[0][0].toString() + board[1][1].toString() + board[2][2].toString();
         dig2 = board[0][2].toString() + board[1][1].toString() + board[2][0].toString();
 
-        ArrayList<String> lines = new ArrayList<String>();
+        this.lines = new ArrayList<String>();
 
         lines.add(row1);
         lines.add(row2);
@@ -46,9 +44,11 @@ public class Board {
         lines.add(col3);
         lines.add(dig1);
         lines.add(dig2);
+
     }
 
     public Boolean isInFavorOfX() {
+
         for (int x = 0; x < lines.size(); x++) {
             if (lines.get(x).equals("XXX"))
                 return true;
@@ -59,9 +59,9 @@ public class Board {
     public Boolean isInFavorOfO() {
         for (int x = 0; x < lines.size(); x++) {
             if (lines.get(x).equals("OOO"))
-                return false;
+                return true;
         }
-        return true;
+        return false;
     }
 
         public Boolean isTie() {
@@ -77,6 +77,7 @@ public class Board {
         }
 
             public String getWinner() {
+
                 for (int x = 0; x < lines.size(); x++) {
                     if (lines.get(x).equals("XXX")) {
                         return "X";
